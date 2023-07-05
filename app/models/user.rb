4eb4ca64ef5792134c7bf552f8626bf12a2b1b76
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-07-05 22:05:29 +0500

    Create Releations between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :categories, foreign_key: :user_id, dependent: :delete_all
+    has_many :transactions, foreign_key: :user_id, dependent: :delete_all
 end
