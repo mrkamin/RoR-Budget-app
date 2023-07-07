df5105cf2d0045ce666c65448dedbcca93e7ffc1
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-07-08 02:00:59 +0500

    Linters fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,18 +11,27 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Full error reports are disabled and caching is turned on.
+  config.action_mailer.default_url_options = { host: 'https://budgeat.onrender.com' }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.elasticemail.com',
+    port: 2525,
+    user_name: 'emeka.ekeoha@email.com',
+    password: 'CDE5E82F3653570E6772275F064DB372A3FF'
+  }
+
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV["RENDER"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -38,7 +47,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
