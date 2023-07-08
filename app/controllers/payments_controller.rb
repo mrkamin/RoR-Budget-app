405e91252026f3cd3307c180ffba9c762d417473
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-07-08 16:52:35 +0500

    Apply review Feedback

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,7 +3,6 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_payment, only: %i[show edit update destroy]
 
-  # GET /payments/1 or /payments/1.json
   def index
     @category = Category.find(params[:category_id])
     @payments = @category.payments.order(created_at: :desc)
@@ -14,20 +13,6 @@ class PaymentsController < ApplicationController
     end
   end
 
-  def show
-    @payment = Payment.find_by(id: params[:id])
-  end
-
-  # GET /payments/new
-  def new
-    @payment = current_user.payments.new
-    @category = Category.find(params[:category_id])
-  end
-
-  # GET /payments/1/edit
-  def edit; end
-
-  # POST /payments or /payments.json
   def create
     @payment = current_user.payments.new(payment_params.merge(user_id: current_user.id).except(:categories))
     params[:categories].each do |key|
@@ -35,14 +20,24 @@ class PaymentsController < ApplicationController
     end
     puts params[:categories]
     if @payment.save
-      # Go back to route
-      redirect_to category_payments_path(params[:category_id]), notice: 'Payment successful.'
+
+      redirect_to category_payments_path(params[:category_id]), notice: 'Payment successful created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /payments/1 or /payments/1.json
+  def new
+    @payment = current_user.payments.new
+    @category = Category.find(params[:category_id])
+  end
+
+  def show
+    @payment = Payment.find_by(id: params[:id])
+  end
+
+  def edit; end
+
   def update
     respond_to do |format|
       if @payment.update(payment_params)
@@ -55,7 +50,6 @@ class PaymentsController < ApplicationController
     end
   end
 
-  # DELETE /payments/1 or /payments/1.json
   def destroy
     @payment.destroy
 
@@ -67,12 +61,11 @@ class PaymentsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_payment
     @payment = Payment.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def payment_params
     params.require(:payment).permit(:name, :amount, :user_id, categories: [])
   end
